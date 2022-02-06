7646c2d7def4d050531cbd597fb9c64906b4c388
Vadim <milewskiy.vadim@gmail.com>
2022-02-06 18:28:38 +0300

    Added entrypoint command for generate application key (if it not presents)

--- build/php/docker-entrypoint.sh
+++ build/php/docker-entrypoint.sh
@@ -10,6 +10,10 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'artisan' ]; then
     composer install --prefer-dist --no-progress --no-interaction
   fi
 
+  if [ -z "${APP_KEY}" ]; then
+    php artisan key:generate
+  fi
+
   if ls -A database/migrations/*.php >/dev/null 2>&1; then
     php artisan migrate
   fi
