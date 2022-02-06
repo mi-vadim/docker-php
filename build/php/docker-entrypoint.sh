40f781c8e346ee3a231968105f0bc0f8782ba1f4
Vadim <milewskiy.vadim@gmail.com>
2022-02-06 17:55:35 +0300

    Added default docker compose

---
+++ build/php/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
+fi
+
+if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'artisan' ]; then
+  if [ "$APP_ENV" != 'local' ] && [ -f "/var/www/composer.json" ]; then
+    composer install --prefer-dist --no-progress --no-interaction
+  fi
+
+  if ls -A database/migrations/*.php >/dev/null 2>&1; then
+    php artisan migrate
+  fi
+fi
+
+exec docker-php-entrypoint "$@"
